1124c0f454edd58d5daf12aa84f07a13c6fe92a1
falafelAlHorno <168140063+falafelAlHorno@users.noreply.github.com>
2024-04-27 19:00:51 -0300

    Add files via upload

---
+++ cargarCadena.cpp
@@ -0,0 +1,10 @@
+void cargarCadena(char *pal, int tam){
+    int i;
+    fflush (stdin); ///limpia el buffer de entrada para que la carga se haga sin caracteres que hayan quedado sin usar
+    for(i=0; i<tam;i++){
+        pal[i]=cin.get();
+        if(pal[i]=='\n')break;
+    }
+    pal[i]='\0';
+    fflush(stdin); ///vuelve a limpiar el buffer para eliminar los caracteres sobrantes
+}
