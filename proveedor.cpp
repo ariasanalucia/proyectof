f963cb4960efb9bbc39a481559573a4f11c91965
narandubafru <theRealSteveGates@outlook.com>
2024-05-22 09:34:25 -0300

    Agregue el cpp de proveedores y movi los diagramas a una carpeta para ordenar

--- proveedor.cpp
+++ proveedor.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
 using namespace std;
+#include "proveedor.h"
+#include "funcionesGlobales.h"
+
+//CONSTRUCTORES
+Producto::Producto(int telefono, int idProveedor, bool estado){
+  _telefono=telefono;
+  _idProveedor=idProveedor;
+  _estado=estado;
+}
+Producto::Producto(){
+  _telefono=0;
+  _idProveedor=0;
+  _estado=true;
+}
+
+//SET
+void Producto::setTelefono(int telefonoNuevo){_telefono = telefonoNuevo;}
+void Producto::setId(int idNuevo){_idProveedor = idNuevo;}
+//void setNombre(const char *nombre){strcpy(_nombreProveedor, nombre);}
+void Producto::setEstado(bool estadoNuevo){_estado = estadoNuevo;}
+
+//GET
+int Producto::getTelefono(){return _telefono;}
+int Producto::getId(){return _idProveedor;}
+char Producto::getNombre(){return _nombreProveedor;}
+bool Producto::getEstado(){return _estado;}
