bd06838c76acf3771aaa452ad4615098917ada4c
narandubafru <theRealSteveGates@outlook.com>
2024-05-23 20:30:40 -0300

    Agregue la clase Hora para las ventas

---
+++ hora.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using na_minpace std;
+#include "hora.h"
+#include <ctime>
+
+ Hora::Hora()
+ {
+   time_t now = time(0);
+   tm* tmPtr = localtime(&now);
+   _min=tmPtr->tm_minute;
+   _hora=tmPtr->tm_hour;
+ }
+
+ void Hora::Mostrar()
+ {
+   cout << (_hora <= 9 ? "0" : "" ) << _hora << ":";
+   cout << (_min <= 9 ? "0" : "" ) << _min << ":";
+ }
+
+ string Hora::toString()
+ {
+   return to_string(_hora) + ":" + to_string (_min);
+ }
---
+++ hora.h
@@ -0,0 +1,16 @@
+#pragma once
+using namespace std;
+
+class Hora 
+ {
+   private:
+    int _hora;
+    int _min;
+
+   public:
+    Hora();
+    //-----------
+    void Mostrar();
+    //-----------
+    string toString();
+ };
