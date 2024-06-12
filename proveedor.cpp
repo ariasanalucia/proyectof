a0e629dc62c86abd68b5963992cb7ecb2d393720
narandubafru <theRealSteveGates@outlook.com>
2024-06-12 14:12:54 -0300

    Agregue Mostrar a la clase Proveedor para que funcione mostrarTodo

--- proveedor.cpp
+++ proveedor.cpp
@@ -26,3 +26,14 @@ int Proveedor::getTelefono(){return _telefono;}
 int Proveedor::getId(){return _idProveedor;}
 const char *Proveedor::getNombre(){return _nombre;}
 bool Proveedor::getEstado(){return _estado;}
+
+//MOSTRAR
+void Proveedor::Mostrar() {
+    if (_estado) {
+        std::cout << "ID: " << _idProveedor << std::endl;
+        std::cout << "Nombre: " << _nombre << std::endl;
+        std::cout << "Telefono: " << _telefono << std::endl;
+        std::cout << std::endl;
+        std::cout << "-------------------------------" << std::endl;
+    }
+}
--- proveedor.h
+++ proveedor.h
@@ -10,13 +10,16 @@
    public:
     Proveedor();
     Proveedor(int telefono, int idProveedor, const char *nombre);
+    //SET
     void setTelefono(int telefonoNuevo);
     void setId(int idNuevo);
     void setNombre(const char *nombre);
     void setEstado(bool estadoNuevo);
+    //GET
     int getTelefono();
     int getId();
     const char *getNombre();
     bool getEstado();
-
+    //MOSTRAR
+    void Mostrar();
  };
